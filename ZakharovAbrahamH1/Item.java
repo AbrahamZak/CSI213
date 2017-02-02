6cfbbb872062e7430b48bd69ab7d9240579bab11
Abraham Zakharov <abrahamzakharov@gmail.com>
2017-02-01 19:42:33 -0500

    Completed and commented on the Item class. Added all methods from UML.

--- ZakharovAbrahamH1/Item.java
+++ ZakharovAbrahamH1/Item.java
@@ -1,5 +1,6 @@
 package rolePlayingGame;
-
+//We create our main class Item which includes the variables for the name of the item,
+//the weight of the item, and the description for when we want to 'examine' the item
 public class Item {
 public String name;
 public int weight;
@@ -9,5 +10,20 @@ public Item (String nameOfItem, int weightOfItem, String descriptionOfItem){
 	name = nameOfItem;
 	weight = weightOfItem;
 	description = descriptionOfItem;
+}
+
+//Method to retrieve the item's name
+public String getName(){
+	return name;
+}
+
+//Method to retrieve the item's weight
+public int getWeight(){
+	return weight;
+}
+
+//Method to retrieve the item's description
+public String examine(){
+	return description;
 }
 }
