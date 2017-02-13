eaa182f6fd6f09ded982dcc51f3bd117de783e7d
Abraham Zakharov <abrahamzakharov@gmail.com>
2017-02-12 22:08:41 -0500

    Added the constructors and an empty method (that will become out recursive square root calculator) to the Babylonian Method class, added some test code to the test class that includes two future test objects.

--- ZakharovAbrahamH2/BabylonianMethod.java
+++ ZakharovAbrahamH2/BabylonianMethod.java
@@ -2,4 +2,26 @@ package babylonianMethod;
 
 public class BabylonianMethod {
 
+//Variables for the number we are finding the square root of and the percent error
+private int squareRoot;
+private double percentError;
+
+/**
+ * Constructor to initialize the number we are finding the square root of and the
+ * given percent error
+ * @param numberChoice
+ * @param percentageError
+ */
+public BabylonianMethod(int numberChoice, double percentageError){
+	squareRoot = numberChoice;
+	percentError = percentageError;
+}
+
+/**
+ * Recursive method to determine the square root with the given percent error
+ * @return squareRootResult
+ */
+public double SquareRoot(){
+	return 0;
+}
 }
--- ZakharovAbrahamH2/BabylonianTestCode.java
+++ ZakharovAbrahamH2/BabylonianTestCode.java
@@ -4,7 +4,17 @@ public class BabylonianTestCode {
 
 	public static void main(String[] args) {
 		
+		//Creates our test object, we will be looking for the square root of
+		//100 with a percent error of 0%
+		BabylonianMethod testRoot = new BabylonianMethod (100,0);
+		//Prints the result of the Babylonian method, should be 10
+		System.out.println(testRoot.SquareRoot());
 
+		//Creates our test object, we will be looking for the square root of
+		//27 with a percent error of 15%
+		BabylonianMethod testRootTwo = new BabylonianMethod (27,15);
+		//Prints the result of the Babylonian method, should be 5.2 +- 15%
+		System.out.println(testRoot.SquareRoot());
 	}
 
 }
