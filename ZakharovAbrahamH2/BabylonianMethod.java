f9de839dfb32e256e5f2f891615b08b2ddba85e4
Abraham Zakharov <abrahamzakharov@gmail.com>
2017-02-13 20:56:44 -0500

    Finished commenting the code, and began initial tests, the program correctly calculates the square root, but the error amount is not functioning correctly

--- ZakharovAbrahamH2/BabylonianMethod.java
+++ ZakharovAbrahamH2/BabylonianMethod.java
@@ -2,11 +2,26 @@ package babylonianMethod;
 
 public class BabylonianMethod {
 
-
+/**
+ * Initial method that starts the recursive function
+ * @param squareRoot
+ * @param errorAmount
+ * @return BabylonianMath(squareRoot, squareRoot/2, errorAmount)
+ */
 public static double SquareRoot(double squareRoot, double errorAmount){
 	return BabylonianMath(squareRoot, squareRoot/2, errorAmount);
 }
 
+/**
+ * Recursive method that will test if our number divided by 2 is the square root
+ * If not it will recursively call itself and test the 
+ * ((testnumber+squareroot number)/testnumber) *0.5
+ * This will continue until the result is within the error amount specified
+ * @param squareRoot
+ * @param testNumber
+ * @param errorAmount
+ * @return 
+ */
 public static double BabylonianMath (double squareRoot, double testNumber, double errorAmount){
 	if ((testNumber*testNumber)-errorAmount<squareRoot  && squareRoot<(testNumber*testNumber)+errorAmount){
 	return testNumber;
--- ZakharovAbrahamH2/BabylonianTestCode.java
+++ ZakharovAbrahamH2/BabylonianTestCode.java
@@ -2,8 +2,23 @@ package babylonianMethod;
 
 public class BabylonianTestCode {
 
+	//Test method for the Babylonian Method Code
 	public static void main(String[] args) {
+		//Test to find the square root of 100 with an error allowance of 1
+		//Should print 10 +- 1
 		System.out.println(BabylonianMethod.SquareRoot(100,1));
+		
+		System.out.println();
+		
+		//Test to find the square root of 54 with an error allowance of 2
+	    //Should print 7.35 +- 2
+		System.out.println(BabylonianMethod.SquareRoot(54,2));
+		
+		System.out.println();
+		
+		//Test to find the square root of 2938461 with an error allowance of 2000
+	    //Should print 1,714.19 +- 2000 
+		System.out.println(BabylonianMethod.SquareRoot(2938461,2000));
 	}
 
 }
