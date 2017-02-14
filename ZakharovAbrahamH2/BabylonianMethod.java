19d4732f2bc06de2671e2c438948af27370374af
Abraham Zakharov <abrahamzakharov@gmail.com>
2017-02-13 21:13:19 -0500

    Testing completed, program functions correctly, ready to ship

--- ZakharovAbrahamH2/BabylonianMethod.java
+++ ZakharovAbrahamH2/BabylonianMethod.java
@@ -24,7 +24,7 @@ public static double SquareRoot(double squareRoot, double errorAmount){
  */
 public static double BabylonianMath (double squareRoot, double testNumber, double errorAmount){
 	if ((testNumber*testNumber)-errorAmount<squareRoot  && squareRoot<(testNumber*testNumber)+errorAmount){
-	return testNumber;
+		return testNumber;
 	}
 	return BabylonianMath (squareRoot, ((testNumber+squareRoot)/testNumber)*0.5, errorAmount);
 }
--- ZakharovAbrahamH2/BabylonianTestCode.java
+++ ZakharovAbrahamH2/BabylonianTestCode.java
@@ -5,20 +5,23 @@ public class BabylonianTestCode {
 	//Test method for the Babylonian Method Code
 	public static void main(String[] args) {
 		//Test to find the square root of 100 with an error allowance of 1
-		//Should print 10 +- 1
+		//The number it prints squared should be 100 +- 1
 		System.out.println(BabylonianMethod.SquareRoot(100,1));
+		//It ends up printing 9.99... which squared = approximately 99.8
 		
 		System.out.println();
 		
 		//Test to find the square root of 54 with an error allowance of 2
-	    //Should print 7.35 +- 2
+		//The number it prints squared should be 54 +- 2
 		System.out.println(BabylonianMethod.SquareRoot(54,2));
+		//It ends up printing 7.21... which squared = approximately 52.02
 		
 		System.out.println();
 		
-		//Test to find the square root of 2938461 with an error allowance of 2000
-	    //Should print 1,714.19 +- 2000 
-		System.out.println(BabylonianMethod.SquareRoot(2938461,2000));
+		//Test to find the square root of 2,938,461 with an error allowance of 300
+		//The number it prints squared should be 2938461 +- 300
+		System.out.println(BabylonianMethod.SquareRoot(2938461,300));
+		//It ends up printing 1714.27... which squared = approximately 2,938,721.63
 	}
 
 }
