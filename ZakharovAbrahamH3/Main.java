7a4e1ca2a6f247b4718ecaad2b481450ff0cb6ca
Abraham Zakharov <abrahamzakharov@gmail.com>
2017-03-03 11:40:16 -0500

    Created an basic test class that creates the list and some test nodes.

---
+++ ZakharovAbrahamH3/Main.java
@@ -0,0 +1,17 @@
+package doublyLinkedList;
+
+public class Main {
+
+	
+	public static void main(String[] args) {
+		//Creates our test doubly linked list
+		DoublyList testList = new DoublyList();
+		
+		//Creates some test nodes
+		Node testNodeOne = new Node ("Hello");
+		Node testNodeTwo = new Node ("Goodbye");
+		Node testNodeThree = new Node ("Shalom");
+
+	}
+
+}
