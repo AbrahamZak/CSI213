aca30d8c9b30e74028ff408794858fd672112cce
Abraham Zakharov <abrahamzakharov@gmail.com>
2017-03-03 11:41:22 -0500

    Added some comments to the node and doubly list classes as well as formatting adjustments and some minor additions.

--- ZakharovAbrahamH3/DoublyList.java
+++ ZakharovAbrahamH3/DoublyList.java
@@ -2,7 +2,9 @@ package doublyLinkedList;
 
 public class DoublyList {
 
+	//Variable to keep track of the size of our doubly list
 	private int size = 0;
+	//Our initial node (which starts empty)
     private Node root = null;
     
 }
--- ZakharovAbrahamH3/Node.java
+++ ZakharovAbrahamH3/Node.java
@@ -2,36 +2,67 @@ package doublyLinkedList;
 
 public class Node {
 
+	//The data our node is holding will be a string
 	private String name;
+	
+	//Each node will reference the next node and the previous node
 	private Node next;
 	private Node previous;
 	
+	/**
+	 * Constructor to initialize a node
+	 * @param name
+	 */
 	public Node(String name) {
 		this.name = name;
 		this.previous = null;
 		this.next = null;
 	}
 	
+	/**
+	 * Getter for the next node
+	 * @return the next node
+	 */
 	public Node getNext() {
 		return next;
 	}
 	
+	/**
+	 * Getter for the previous node
+	 * @return the previous node
+	 */
 	public Node getPrevious() {
 		return previous;
 	}
 	
+	/**
+	 * Setter for the next node
+	 * @param node
+	 */
 	public void setNext(Node node) {
 		this.next = node;
 	}
 	
+	/**
+	 * Setter for the previous node
+	 * @param node
+	 */
 	public void setprevious(Node node) {
 		this.previous = node;
 	}
 	
+	/**
+	 * Getter for the name
+	 * @return name
+	 */
 	public String getName() {
 		return name;
 	}
 
+	/**
+	 * Setter for the name
+	 * @param name
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
