e2c76cef6c7c4954553ec0507fab7ed2f0fd2c13
Abraham Zakharov <abrahamzakharov@gmail.com>
2017-03-16 00:27:24 -0400

    Implemented a tail node that keeps track of the last item on the list.

--- ZakharovAbrahamH3/DoublyList.java
+++ ZakharovAbrahamH3/DoublyList.java
@@ -6,6 +6,7 @@ public class DoublyList {
 	private int size = 0;
 	//Our initial node (which starts empty)
     private Node root = null;
+    private Node tail = null;
     
     /**
      * Method to add a node to the doubly list
@@ -19,6 +20,7 @@ public class DoublyList {
     	 else if (root == null){
     		 size++;
     		 root = node;
+    		 tail = node;
     	 }
     	 else{
     		 Node current = root;
@@ -27,7 +29,8 @@ public class DoublyList {
                  }
                  size++;
                  current.setNext(node);
-                 node.setPrevious(current); 
+                 node.setPrevious(current);
+                 tail = node;
     	 }
     		 
     }
@@ -65,39 +68,33 @@ public class DoublyList {
      * Start with the tail and traverse till you reach null. Print out the data in the nodes.
      */
     public void iterateBackwards(){
-    	//set a temp node equal to our root
-          Node currentNode = root;
+    	
+    	//set a temp node equal to our tail
+          Node currentNode = tail;
           
-        //If the list is empty, let the user know you can't iterate it
+          //If the list is empty, let the user know you can't iterate it
           if (currentNode==null){
         	  System.out.println("The list is empty.");
         	  return;
           }
           
-          //Continue looping through the list until we get to the end
-          while(currentNode.getName()!= null)
-          {
-        	  currentNode = currentNode.getNext();
+        //Continue looping through the list backwards until we get to the end,
+          //printing each node in the process
+          while (currentNode.getName()!=null){
         	  
-        	  //If the next node is empty we break the loop
-        	  if (currentNode.getNext()==null){
-        		  break;
-        	  }
-          }
-          
-          //While the previous node isn't empty we print and then move back one
-          while (currentNode.getName()!= null){
-        	  //Print the current node
+        	//Print the current node
         	  System.out.println(currentNode.getName());
         	  
-        	  //If the previous is empty, we break the loop
-              if (currentNode.getPrevious()==null){
-    		  break;
-    	      }
-              
-              //Otherwise we set our node to the previous one
-          	  currentNode = currentNode.getPrevious();
+        	//If the previous node is empty we break the loop
+        	  if (currentNode.getPrevious()==null){
+        		  break;
+        	  }
+        	  
+        	//Get the previous node
+        	  currentNode = currentNode.getPrevious();
+        	  
           }
+       
               
           }
     /**
