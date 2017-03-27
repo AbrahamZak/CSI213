f8f11882f83973b95074f54e0b81c03be7c701d2
Abraham Zakharov <abrahamzakharov@gmail.com>
2017-03-27 19:54:29 -0400

    Completed the stack class, still being tested, all method pop, push, and peek have been completed

--- ZakharovAbrahamH4/Stack.java
+++ ZakharovAbrahamH4/Stack.java
@@ -1,5 +1,39 @@
 package stacksAndQueuesADT;
 
-public class Stack {
 
+public class Stack {
+	//Our initial node (which starts empty)
+    private Node root = null;
+    
+    /**
+     * Method to add a node to the beginning of our stack
+     * @param add
+     */
+    public void push(String add) {
+    	 Node newNode = new Node (add);
+    	//If the stack is empty, add this initial node as the root
+   	     if (root == null){
+   		 root = newNode;
+   	     }
+   	     //If the stack is not empty, make the new node the root
+   		 else{
+   			 newNode.setNext(root);
+   			 root = newNode;
+   		 }	  
+   	  }
+    
+    /**
+     * Method to remove the top node of our stack
+     */
+    public void pop() {
+    	root = root.getNext();
+    }
+    
+    /**
+     * Method to get the data from the top node of our stack
+     */
+    public String peek() {
+    	String peek = root.getName();
+    	return peek;
+    }
 }
