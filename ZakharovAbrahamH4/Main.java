ae5873c49e4e3939c1bd2bae315999a7a12a49f2
Abraham Zakharov <abrahamzakharov@gmail.com>
2017-03-28 14:20:46 -0400

    Wrote the methods to move from queue to stack, stack to queue, and stack to stack. Still need to implement test code as well as thoroughly examine the code to make sure it will work in all instances

--- ZakharovAbrahamH4/Main.java
+++ ZakharovAbrahamH4/Main.java
@@ -36,6 +36,61 @@ public class Main {
 		System.out.println(testQueue.peek());
 		//Print our Queue, should only contain string "Adios"
 		testQueue.print();
+		
+		
+		
+	}
+	/**
+	 * Method to move a stack to a queue
+	 * @param toQueue
+	 * @param fromStack
+	 */
+	public void stackToQueue(Stack toQueue, Queue fromStack){
+		//Loop through the stack until it is empty
+		while (toQueue !=null){
+			//Enqueue the top of the stack, then pop the stack
+			fromStack.enqueue(toQueue.peek());
+			toQueue.pop();
+		}
+	}
+	
+	/**
+	 * Method to move a queue to a stack
+	 * @param toStack
+	 * @param fromQueue
+	 */
+	public void QueuetoStack(Queue toStack, Stack fromQueue){
+		//Loop through the queue until it is empty
+		while (toStack !=null){
+			//Push the end of the queue, then dequeue the queue
+			fromQueue.push(toStack.peek());
+			toStack.dequeue();
+		}
+	}
+
+	/**
+	 * Method to move one stack to another stack
+	 * @param a
+	 * @param b
+	 */
+	public void stackToStack(Stack a, Stack b){
+		//Since stacks are LIFO we need to first move our first stack to a temp stack
+		//Then move that temp stack into our second stack
+		Stack temp = new Stack();
+		
+		//Loop through the first stack until it is empty
+		while (a!=null){
+			//Push to the temp stack then pop from the original
+			temp.push(a.peek());
+			a.pop();
+		}
+		
+		//Loop through the temp stack until it is empty
+		while (temp!=null){
+			//Push to the second stack then pop from the temp
+			b.push(temp.peek());
+			temp.pop();
+		}
 	}
 
 }
