3a8fdbffb134ce3d3dcc730c07f649c079ef7747
Abraham Zakharov <abrahamzakharov@gmail.com>
2017-03-27 20:18:23 -0400

    Made minor alterations to current completed methods to print what they are doing, also completed the dequeue method.

--- ZakharovAbrahamH4/Queue.java
+++ ZakharovAbrahamH4/Queue.java
@@ -10,36 +10,60 @@ public class Queue {
      * @param add
      */
     public void enqueue(String add) {
+    //Create the node we will be adding from the string given
+   	 Node newNode = new Node (add);
+   	 
+   	//If the queue is empty, add this initial node as the root
+  	     if (root == null){
+  		 root = newNode;
+  		 System.out.println("enqueue (" + add + ")");
+  	     }
+  	     
+  	     //If the queue is not empty, make the new node the root
+  		 else{
+  			 newNode.setNext(root);
+  			 root = newNode;
+  			System.out.println("enqueue (" + add + ")");
+  		 }	  
     	
-    	//Create the node we will be adding from the string given
-    	 Node newNode = new Node (add);
-    	 
-    	//If the queue is empty, add this initial node as the root
-   	     if (root == null){
-   		 root = newNode;
-   	     }
-   	     
-   	     //If the queue is not empty, place the new node at the end of the list
-   		 else{
-   			 
-   		//set a temp node equal to our root
-   			Node currentNode = root;
-   			
-   		//Continue looping through the queue until we get to the end	
-   		 while(currentNode.getName()!= null)
-         {
-
-       	  //If the next node is empty we break the loop
-       	  if (currentNode.getNext()==null){
-       		  break;
-       	  }
-       	  
-       	  //Get the next node
-             currentNode = currentNode.getNext();
-         }
-   		 
-   		 //Set our new node to the last node in the queue
-   		 currentNode.setNext(newNode);
-   		 }	  
    	  }
+    
+    /**
+     * Method to remove the node at the end of our queue
+     */
+    public void dequeue() {
+    	//If there is only one item in our queue, remove that one item
+    	if (root.getNext()==null){
+    		System.out.println("dequeue (" + root.getName() + ")");
+    		root = null;
+    		return;
+    	}
+    	
+    	//If there is more than one item, iterate our queue until we reach the end and
+    	//remove that item from the queue
+    	else{
+    		//set a temp node equal to our root
+    		Node currentNode = root;
+    		//Continue looping through the queue until we get to the end	
+    		while(currentNode.getName()!= null)
+    		{
+
+    		//If the next node is empty we break the loop
+    		if (currentNode.getNext()==null){
+    		break;
+    		}
+    		
+    		//Get the next node
+    		currentNode = currentNode.getNext();
+    		}
+    		
+    		//Once we are looking at the last node of the list,
+    		//set that node equal to null
+    		System.out.println("dequeue (" + currentNode.getName() + ")");
+    		currentNode = null;
+    
+    	}
+    }
+    
+    
 }
--- ZakharovAbrahamH4/Stack.java
+++ ZakharovAbrahamH4/Stack.java
@@ -17,12 +17,14 @@ public class Stack {
     	//If the stack is empty, add this initial node as the root
    	     if (root == null){
    		 root = newNode;
+   		 System.out.println("push (" + add + ")");
    	     }
    	     
    	     //If the stack is not empty, make the new node the root
    		 else{
    			 newNode.setNext(root);
    			 root = newNode;
+   			System.out.println("push (" + add + ")");
    		 }	  
    	  }
     
@@ -30,7 +32,16 @@ public class Stack {
      * Method to remove the top node of our stack
      */
     public void pop() {
+    	//If there is only one item in our stack, remove that one item
+    	if (root.getNext()==null){
+    		System.out.println("pop -> " + root.getName());
+    		root = null;
+    		return;
+    	}
+    	else{
+    	System.out.println("pop -> " + root.getName());
     	root = root.getNext();
+    	}
     }
     
     /**
