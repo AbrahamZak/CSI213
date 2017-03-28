7a4744a96086fb34365aab5e52edcd1ad98c3b26
Abraham Zakharov <abrahamzakharov@gmail.com>
2017-03-27 20:02:23 -0400

    Began work on the Queue class, created and completed the enqueue method.

--- ZakharovAbrahamH4/Queue.java
+++ ZakharovAbrahamH4/Queue.java
@@ -2,4 +2,44 @@ package stacksAndQueuesADT;
 
 public class Queue {
 
+	//Our initial node (which starts empty)
+    private Node root = null;
+    
+    /**
+     * Method to add a node to the beginning of our queue
+     * @param add
+     */
+    public void enqueue(String add) {
+    	
+    	//Create the node we will be adding from the string given
+    	 Node newNode = new Node (add);
+    	 
+    	//If the queue is empty, add this initial node as the root
+   	     if (root == null){
+   		 root = newNode;
+   	     }
+   	     
+   	     //If the queue is not empty, place the new node at the end of the list
+   		 else{
+   			 
+   		//set a temp node equal to our root
+   			Node currentNode = root;
+   			
+   		//Continue looping through the queue until we get to the end	
+   		 while(currentNode.getName()!= null)
+         {
+
+       	  //If the next node is empty we break the loop
+       	  if (currentNode.getNext()==null){
+       		  break;
+       	  }
+       	  
+       	  //Get the next node
+             currentNode = currentNode.getNext();
+         }
+   		 
+   		 //Set our new node to the last node in the queue
+   		 currentNode.setNext(newNode);
+   		 }	  
+   	  }
 }
--- ZakharovAbrahamH4/Stack.java
+++ ZakharovAbrahamH4/Stack.java
@@ -10,11 +10,15 @@ public class Stack {
      * @param add
      */
     public void push(String add) {
+    	
+    	//Create the node we will be adding from the string given
     	 Node newNode = new Node (add);
+    	 
     	//If the stack is empty, add this initial node as the root
    	     if (root == null){
    		 root = newNode;
    	     }
+   	     
    	     //If the stack is not empty, make the new node the root
    		 else{
    			 newNode.setNext(root);
