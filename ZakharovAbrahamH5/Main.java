35fe8b6c22d6a5843ac212155b11c798a6db4f4d
Abraham Zakharov <abrahamzakharov@gmail.com>
2017-04-16 15:29:00 -0400

    Created method to insert a user specified amount of random numbers into the list, in the test code I added 10 random variables to the list and then iterated the list forwards

--- ZakharovAbrahamH5/Main.java
+++ ZakharovAbrahamH5/Main.java
@@ -1,15 +1,35 @@
 package bubbleSort;
+import java.util.Random;
 
 public class Main {
 
 	public static void main(String[] args) {
+		//Create our list
 		DoublyList testList = new DoublyList();
-		testList.add(1);
-		testList.add(5);
-		testList.add(7);
-		testList.add(15);
+		
+		//Add ten random numbers to the list
+		randomFill(10, testList);
+		
+		//Iterate the list
 		testList.iterateForwards();
-
 	}
 
+	/**
+	 * Method to fill the list with a specified amount of random integers
+	 * that can go upto 100
+	 * @param amount
+	 * @param list
+	 */
+	public static void randomFill(int amount, DoublyList list){
+		//Create a new random variable
+		Random randomNum = new Random(); 
+		
+		//loop for the specified amount of times
+		for (int i=0; i<amount; i++){
+			//Continue adding random integers to the list until the user's
+			//specified amount is reached
+		   int randomResult = randomNum.nextInt(100); 
+		   list.add(randomResult);
+		}
+	}
 }
